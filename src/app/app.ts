c970448afb268b9bbbaf564c79b81cbd36514731
Swetha Sri Ayinam <swethasriayinam7@gmail.com>
2025-11-24 23:10:44 +0530

    chnage in all

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('list');
+  task='';
+  taskList:{id:number,task:string}[]=[]
+
+  addTask(){
+    this.taskList.push({id:this.taskList.length+1, task:this.task})
+    this.task=''
+
+    console.log(this.taskList);
+  }
+
+  deleteTask(taskId:number){
+    this.taskList=this.taskList.filter((item)=>item.id!=taskId);
+  }
 }
